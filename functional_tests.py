1979d81d901ff02142dc28b51eade4265e5f1c9c
TonyLuo <luodongyi_career@163.com>
2018-07-01 20:27:16 +0800

    Ready to restructure to adapt to multiple tasks

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -28,13 +29,24 @@ class NewVisitorTest(unittest.TestCase):
 
         #Press Enter button and the page refreshes
         inputbox.send_keys(Keys.ENTER)
+        
+        time.sleep(3)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers' , [row.text for row in rows])
+
+        #She inputs another item "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), 
-        "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers' , [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly' , [row.text for row in rows])
 
-        #She inputs another item "Use peacock feathers to make a fly"
+
 
 
         self.fail('Finish the test')
