5bcfacd97ea5d1a1a9b719b8ce6a8aa9aef24626
TonyLuo <luodongyi_career@163.com>
2018-07-05 07:23:56 +0800

    add a second user for test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -35,8 +35,9 @@ class NewVisitorTest(LiveServerTestCase):
 
         #Press Enter button and the page refreshes
         inputbox.send_keys(Keys.ENTER)
-        
         time.sleep(3)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #She inputs another item "Use peacock feathers to make a fly"
@@ -48,10 +49,33 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
+        #a new user visits the website
+        ## we use a new browser dialog
+        ## the first user's information will not leak from the cookie
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # he inputs a new to-do item and create a list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #he gets the unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #This page does not have her list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+
 
 
 
         self.fail('Finish the test')
+        #She visits the URL and the TO-DO list is there
 
 # if __name__ == '__main__':
 #     unittest.main()
