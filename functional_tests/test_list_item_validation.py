b5c6dc0996dc439beb9cabf1e02c9bb20e72c330
Tony 罗东艺 <LUODY1@cn.midea.com>
2019-08-16 10:34:59 +0800

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,7 +5,7 @@ import time
 
 class ItemValidationTest(FunctionalTest):
     
-    @skip
+    #@skip
     def test_cannot_add_empty_list_items(self):
         #The user submits an empty list item
         self.browser.get(self.live_server_url)
@@ -14,11 +14,14 @@ class ItemValidationTest(FunctionalTest):
         time.sleep(5)
 
         #The error message shows the list item cannot be empty
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item")
+        #error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.browser.find_element_by_css_selector('#id_text:invalid')
+        #self.assertEqual(error.text, "You can't have an empty list item")
+        self.assertEqual(error.text, "")
 
         #The user submits a normal list item and it works
-        self.get_item_input_box().send_keys('Buy milk\n')
+        self.get_item_input_box().send_keys('Buy milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
 
         #The user submits an empty list item again
@@ -26,11 +29,15 @@ class ItemValidationTest(FunctionalTest):
 
         #The user sees the error message again
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You can't have an empty list item")
+        #error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.browser.find_element_by_css_selector('#id_text:invalid')
+        #self.assertEqual(error.text, "You can't have an empty list item")
+        self.assertEqual(error.text, "")
 
         #Input the characters and it works
-        self.get_item_input_box().send_keys('Make tea\n')
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        time.sleep(5)
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
 
